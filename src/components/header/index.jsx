e61fdfdf248684e94da2e65f3e8ef78f4f03584e
Админ <macbahodir@gmail.com>
2024-10-29 21:06:45 +0500

    boshlangich papkalar

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Header() {
   return (
-    <div>Header</div>
+    <header className="header">
+      <div className="container">
+        <div className="header-row">
+          <div className="header-logo">
+            <h1>Logo</h1>
+          </div>
+          <nav className="header-nav">
+            <ul className="header-links">
+              
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </header>
   )
 }
 
